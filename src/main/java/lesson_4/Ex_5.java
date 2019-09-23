9ba83a3c8449162fc53fcaca54fada5c4f48fec6
RenRevan <funnyincident28@gmail.com>
2019-09-23 20:55:59 +0300

    add exercises to lesson 4

--- src/main/java/lesson_4/Ex_5.java
+++ src/main/java/lesson_4/Ex_5.java
@@ -4,19 +4,19 @@ public class Ex_5 {
     public static void main(String[] arg){
         int sum;
         sum= min(2,4,6,8);
-        System.out.print("Sum = "+sum);
+        System.out.print("Min = "+sum);
 
     }
 
     public static int min(int a, int b, int c, int d){
-        int s1;
-        s1 = min(a, b)+c+d;
-        return s1;
+//
+        int min1 = min(a, b);
+        int min2 = min(c,d);
+
+        return min(min1, min2);
     }
 
     public static int min(int a, int b){
-        int s2;
-        s2= a+b;
-        return s2;
+        if(a<b){return a;}else{return b;}
     }
 }
